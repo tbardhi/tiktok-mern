29b0cf391c4b0c95a8e7bf4ed773ebf5ab1eb580
LAPTOP-3SO9FEO7\user <taulantbardhi2017@gmail.com>
2024-04-09 18:09:16 +0200

    first commit

---
+++ tiktok/src/App.js
@@ -0,0 +1,37 @@
+import axios from './axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+import Video from './Video';
+
+function App() {
+  const [ videos, setVideos ] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+      return response;
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__videos">
+        { videos.map(({ url, channel, description, song, likes, messages, shares }) => (
+          <Video
+            url={ url }
+            channel={ channel }
+            description={ description }
+            song={ song }
+            likes={ likes }
+            messages={ messages }
+            shares={ shares }
+          />
+        )) }
+      </div>
+    </div>
+  );
+}
+
+export default App;
